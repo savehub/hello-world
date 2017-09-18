60ff919e9b99a8af8a2b1db6254e16d46cde2a19
savehub <32055792+savehub@users.noreply.github.com>
2017-09-18 16:30:29 +0800

    Add file
    Add a local file testlocaladdfile.cpp

---
+++ testlocaladdfile.cpp
@@ -0,0 +1,20 @@
+#include <opencv2/opencv.hpp>
+using namespace std;
+using namespace cv;
+int main(int argc, char* argv[])
+{
+    const char* imagename = "D:/VScode/LearnOpenCV/testimage/Fruits.jpg";
+    //从文件中读入图像
+    Mat img = imread(imagename);
+    //如果读入图像失败
+    if(img.empty())
+    {
+        fprintf(stderr, "Can not load image %s\n", imagename);
+        return -1;
+    }
+    //显示图像
+    imshow("image", img);
+    //此函数等待按键，按键盘任意键就返回
+    waitKey();
+    return 0;
+}
